7e6d950c6226a49f2962f346aac0c9554669da1d
glamus <it@patrickmichiels.de>
2021-10-14 18:45:42 +0200

    updated SimplEUsage.php

--- examples/simpleUsage.php
+++ examples/simpleUsage.php
@@ -1,26 +1,33 @@
 <?php
 
-// Make sure to have included this project with composer or cloned from git.
-// For more see README.md
+/**
+ * @file
+ * This file shows the basic usage of glamus_utils using examples.
+ */
 
-
-// You can use abbreviations at the start of file, so you can use the shorter
-// form of the class
+// You should use abbreviations at the start of file, so you can use the shorter
+// form of the class.
+use Glamus\Utils\StringUtils;
 use Glamus\Utils\FileUtils;
 
-// require or include the install.php.
 
+// Option 1: composer (recommended).
+$pathToVendor = dirname(dirname(dirname(dirname(__DIR__))));
+require_once $pathToVendor . "/vendor/autoload.php";
+
+// Option 2: git Require or include the install.php if cloned via git.
 // Change this path to where you cloned or installed the project to.
-$pathToGlamusUtilsRoot = dirname(dirname (__DIR__));
-require_once $pathToGlamusUtilsRoot . "/glamus_utils_test/install.php";
+$pathToGlamusUtilsRoot = dirname(dirname(__DIR__));
+require_once $pathToGlamusUtilsRoot . "/glamus_utils_test/install/install.php";
 
-// Short usage when having used abbreviation at start.
+
+// Short usage when having used abbreviation at start (recommended).
 $fileUtils = new FileUtils();
-echo $fileUtils->echoPhrase("It's really working with use");
+echo $fileUtils->echoPhrase("Test output");
 
 echo "\n<br/>";
 echo "\n<br/>";
 
-// Longer instantiation, if you do not want to use "use" at start.
-$stringUtils = new \Glamus\Utils\StringUtils();
-echo $stringUtils->echoPhrase("It's really working with use");
+// Longer instantiation, if you  cannot to use "use" at start.
+$stringUtils = new Glamus\Utils\StringUtils();
+echo $stringUtils->echoPhrase("Test output using use");
